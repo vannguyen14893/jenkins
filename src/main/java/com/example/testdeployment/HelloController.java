e0e06541f20a466838eb76f487af5abb2e2ca2fe
ducvan14893 <ducvan14893@gmail.com>
2020-08-21 11:34:10 +0700

    ok

--- src/main/java/com/example/testdeployment/HelloController.java
+++ src/main/java/com/example/testdeployment/HelloController.java
@@ -10,6 +10,6 @@ public class HelloController {
     private String email;
     @GetMapping
     public String hello(){
-        return "admin_888888";
+        return "fffffffffffffffffffff";
     }
 }
